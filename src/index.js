2893f2dd1bfaae4a8fc94e006d9eb93881cc0c88
nishantsinghsage <nishant.singh@sage.com>
2025-05-24 10:59:20 +0530

    commit1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express= require('express');
+const app = express();
+require('dotenv').config()
+
+const PORT= 3000
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(process.env.PORT,()=>console.log(`SERVER STARTED ON PORT: ${PORT} ..`))
